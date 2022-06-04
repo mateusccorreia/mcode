bc06683fca4d13e9bb1b289c1f12b6e21f1f9153
Mateus Correia <mateuscorreia.dev@gmail.com>
2022-06-04 16:20:38 -0300

    Added some styles components in the project

--- pages/services.js
+++ pages/services.js
@@ -1,14 +1,19 @@
+import Head from 'next/head';
 import Link from 'next/link';
+import Layout from '../src/components/layout';
 
 export default function Services() {
     return (
-        <>
+        <Layout>
+            <Head>
+                <title>Our Services</title>
+            </Head>
             <h1>Services</h1>
             <h2>
                 <Link href="/">
                     <a>Voltar</a>
                 </Link>
             </h2>
-            </>
+        </Layout>
     )
 }
